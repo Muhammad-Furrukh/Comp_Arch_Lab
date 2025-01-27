f1d161af6df36a379b3245aa01972ddda81fa3f6
Muhammad-Furrukh <mfurrukh12082002@gmail.com>
2025-01-28 00:34:06 +0500

    Update UART_Tx.sv

--- UART_Tx/UART_Tx.sv
+++ UART_Tx/UART_Tx.sv
@@ -7,6 +7,6 @@ CHANGE_BIT = 3'b100, CONT_BIT = 3'b101;
 
 logic [7:0] data, fifo_data;
 logic [13:0] baud_divisor_r, baud_count;
-logic fifo_load, parity_bit, shift_en, load_en, Tx_sel, bit_count;
-
+logic fifo_load, parity_bit, shift_en, load_en, Tx_sel, bit_count, Odd_parity_r, count_en,
+Tx_en_r, baud_eq, fifo_status, Two_stop_r, bit_eq, Tx_status;
 endmodule

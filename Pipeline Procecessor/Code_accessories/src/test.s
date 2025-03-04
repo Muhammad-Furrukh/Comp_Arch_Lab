5fd1172cffc843d5ddf46e5838b1c607ecfb8c29
Muhammad-Furrukh <120235607+Muhammad-Furrukh@users.noreply.github.com>
2025-03-04 06:40:28 +0500

    Add files via upload

---
+++ Pipeline Procecessor/Code_accessories/src/test.s
@@ -0,0 +1,7 @@
+lui x8, 0x80000
+addi x5, x0, 10
+addi x6, x0, 20
+sw x7, 0(x8)
+andi x10, x9, 15
+add x7, x5, x6
+lw x9, 0(x8)
---
+++ Pipeline Procecessor/instruction_memory.sv
@@ -0,0 +1,16 @@
+module instruction_memory#(
+  parameter mem_size = 1024
+ )
+  (input logic [31:0] addr,
+  output logic [31:0] instr);
+  reg [31:0] mem [0:mem_size-1]; 
+  initial begin
+    $readmemh("C:/Users/PMLS/Desktop/main.txt", mem);
+    
+  end
+  
+  //Converting byte address into word address
+  always_comb begin
+    instr = mem[addr >> 2];
+  end
+endmodule

3f83aed55f16e8bb469cb46c0d31cd1cef2a9a9a
Muhammad-Furrukh <mfurrukh12082002@gmail.com>
2025-03-26 02:58:02 +0500

    Made slight modification in pointer for FIFO.

--- UART_Tx/Tx_FIFO.sv
+++ UART_Tx/Tx_FIFO.sv
@@ -4,8 +4,14 @@ module Tx_FIFO(
     input logic        clk,
     input logic        reset,
     input logic        fifo_load,
+    input logic        load_en,
     output logic [7:0] fifo_data
 );
 
 logic [7:0] fifo [7:0];
+always_ff @(posedge clk) begin
+    if (reset) begin
+        fifo <= '{ 8{8'hx} };
+    end
+end
 endmodule
